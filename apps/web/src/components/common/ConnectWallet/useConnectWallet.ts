d09beca1955d90c98a7a89e42cf4a0e2ec2a27c8
shoom3301 <shoom3301@gmail.com>
2026-06-15 18:44:02 +0500

    chore: fix build

--- apps/web/src/components/common/ConnectWallet/useConnectWallet.ts
+++ apps/web/src/components/common/ConnectWallet/useConnectWallet.ts
@@ -4,8 +4,6 @@ import useOnboard, { connectWallet } from '@/hooks/wallets/useOnboard'
 const useConnectWallet = () => {
   const onboard = useOnboard()
 
-  console.log('onboard', onboard)
-
   return useCallback(() => {
     if (!onboard) {
       return Promise.resolve(undefined)
